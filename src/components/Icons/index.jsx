51247d71dbb785f89f3a763420749d28f4cc7447
Nguyen Van Dao <nvdao003@gmail.com>
2025-01-21 16:00:12 +0700

    responsive page

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -316,3 +316,22 @@ export const LinkIcon = ({ className, width, height }) => (
     <path d='M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71' />
   </svg>
 )
+
+export const MenuIcon = ({ className, width, height }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={width}
+    height={height}
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className={className}
+  >
+    <line x1='4' x2='20' y1='12' y2='12' />
+    <line x1='4' x2='20' y1='6' y2='6' />
+    <line x1='4' x2='20' y1='18' y2='18' />
+  </svg>
+)
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,70 +7,94 @@ import {
   GraducationIcon,
   HomeIcon,
   LayerGroupIcon,
+  MenuIcon,
   NoteIcon,
   UserFollowingIcon
 } from '../Icons'
 import './Sidebar.scss'
 
 function Sidebar() {
+  const handleMenuToggle = (e) => {
+    const menu = document.querySelector('.navToggle')
+    const aside = document.querySelector('.aside')
+
+    document.addEventListener('click', () => {
+      if (menu.contains(e.target)) {
+        aside.classList.toggle('activeAside')
+        menu.classList.toggle('activeNavToggle')
+      } else {
+        if (!aside.contains(e.target)) {
+          aside.classList.remove('activeAside')
+          menu.classList.remove('activeNavToggle')
+        }
+      }
+    })
+  }
+
   return (
-    <div className='aside'>
-      <a href='#home' className='navbarLogo'>
-        <img src={Logo} alt='Logo' />
-      </a>
+    <>
+      <div className='aside'>
+        <a href='#home' className='navbarLogo'>
+          <img src={Logo} alt='Logo' />
+        </a>
 
-      <nav className='nav'>
-        <div className='navMenu'>
-          <ul className='navList'>
-            <li className='navItem'>
-              <a href='/' className='navLink'>
-                <HomeIcon className='iconSidebar' />
-              </a>
-            </li>
+        <nav className='nav'>
+          <div className='navMenu'>
+            <ul className='navList'>
+              <li className='navItem'>
+                <a href='/' className='navLink'>
+                  <HomeIcon className='iconSidebar' />
+                </a>
+              </li>
 
-            <li className='navItem'>
-              <a href='#about' className='navLink'>
-                <UserFollowingIcon className='iconSidebar' />
-              </a>
-            </li>
+              <li className='navItem'>
+                <a href='#about' className='navLink'>
+                  <UserFollowingIcon className='iconSidebar' />
+                </a>
+              </li>
 
-            <li className='navItem'>
-              <a href='#services' className='navLink'>
-                <BrieftCaseIcon className='iconSidebar' />
-              </a>
-            </li>
+              <li className='navItem'>
+                <a href='#services' className='navLink'>
+                  <BrieftCaseIcon className='iconSidebar' />
+                </a>
+              </li>
 
-            <li className='navItem'>
-              <a href='#resume' className='navLink'>
-                <GraducationIcon className='iconSidebar' />
-              </a>
-            </li>
+              <li className='navItem'>
+                <a href='#resume' className='navLink'>
+                  <GraducationIcon className='iconSidebar' />
+                </a>
+              </li>
 
-            <li className='navItem'>
-              <a href='#portfolio' className='navLink'>
-                <LayerGroupIcon className='iconSidebar' />
-              </a>
-            </li>
+              <li className='navItem'>
+                <a href='#portfolio' className='navLink'>
+                  <LayerGroupIcon className='iconSidebar' />
+                </a>
+              </li>
 
-            <li className='navItem'>
-              <a href='#blog' className='navLink'>
-                <NoteIcon className='iconSidebar' />
-              </a>
-            </li>
+              <li className='navItem'>
+                <a href='#blog' className='navLink'>
+                  <NoteIcon className='iconSidebar' />
+                </a>
+              </li>
 
-            <li className='navItem'>
-              <a href='#contact' className='navLink'>
-                <BubbleIcon className='iconSidebar' />
-              </a>
-            </li>
-          </ul>
+              <li className='navItem'>
+                <a href='#contact' className='navLink'>
+                  <BubbleIcon className='iconSidebar' />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className='navFooter'>
+          <span className='navCopyright'>&copy; 2024 - 2025.</span>
         </div>
-      </nav>
-
-      <div className='navFooter'>
-        <span className='navCopyright'>&copy; 2024 - 2025.</span>
       </div>
-    </div>
+
+      <button className='navToggle' onClick={handleMenuToggle}>
+        <MenuIcon className='icon navToggleIcon' />
+      </button>
+    </>
   )
 }
 
